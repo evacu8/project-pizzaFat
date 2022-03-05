6fb2c5f753ad9491ad3c0c2fd8125511e98235e0
Krzysztof Potocki <kpotocki89@gmail.com>
2022-03-05 12:56:02 +0100

    Fix initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -94,10 +94,10 @@
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
-        const activeProduct = document.querySelector('.product__header .active');
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if (activeProduct && activeProduct !== thisProduct.element){
-          thisProduct.element.classList.remove('active');
+        if (activeProduct && activeProduct != thisProduct.element){
+          activeProduct.classList.remove('active');
         }
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
