81a568a462afe8121115181d9fd5405dd4ab94b0
Krzysztof Potocki <kpotocki89@gmail.com>
2022-03-19 21:34:23 +0100

    Add AmountWidgets for class Booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import { templates } from '../settings.js';
+import { select, templates } from '../settings.js';
+import AmountWidget from './AmountWidget.js';
 
 class Booking {
   constructor(element){
@@ -17,6 +18,25 @@ class Booking {
 
     thisBooking.dom.wrapper = element;
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
+    
+    thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+  }
+
+  initWidgets(){
+    const thisBooking = this;
+
+    thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
+    thisBooking.dom.peopleAmount.addEventListener('updated', function(){
+      thisBooking.peopleAmount = thisBooking.peopleAmountWidget.value;
+      console.log('peopleAmount', thisBooking.peopleAmount);
+    });
+
+    thisBooking.hoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('updated', function(){
+      thisBooking.hoursAmount = thisBooking.hoursAmountWidget.value;
+      console.log('hoursAmount', thisBooking.hoursAmount);
+    });
   }
 
 }
