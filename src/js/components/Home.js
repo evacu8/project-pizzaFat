efca22cc3af3c0f8807ea05af23477630d459a85
Krzysztof Potocki <kpotocki89@gmail.com>
2022-03-26 09:18:52 +0100

    Add Home.render()

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,16 +1,26 @@
+import { templates } from '../settings.js';
+
 class Home{
-  constructor(){
+  constructor(element){
     const thisHome = this;
-    thisHome.render();
+    thisHome.render(element);
     thisHome.initWidgets();
   }
 
-  render(){
+  render(element){
+    const thisHome = this;
 
+    const generatedHTML = templates.homePage();
+    thisHome.dom = {};
+
+    thisHome.dom.wrapper = element;
+    thisHome.dom.wrapper.innerHTML = generatedHTML;
+    // console.log(thisHome.dom.wrapper);
+    // console.log(thisHome.dom.wrapper.innerHTML);
   }
 
   initWidgets(){
-    
+
   }
 }
 
