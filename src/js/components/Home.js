858c089f02dbe9ae65a9fd9f459887e74f5734bf
Krzysztof Potocki <kpotocki89@gmail.com>
2022-03-27 20:00:53 +0200

    Add order/book links functionality

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,4 +1,4 @@
-import { select, templates} from '../settings.js';
+import { select, templates, classNames} from '../settings.js';
 import utils from '../utils.js';
 import Carousel from './Carousel.js';
 
@@ -7,6 +7,7 @@ class Home{
     const thisHome = this;
     thisHome.render(element);
     thisHome.initWidgets();
+    thisHome.initActions();
   }
 
   render(element){
@@ -21,13 +22,40 @@ class Home{
 
     thisHome.dom.wrapper = element;
     thisHome.dom.carouselWrapper = document.querySelector(select.containerOf.carousel);
-
+    thisHome.pages = document.querySelector(select.containerOf.pages).children;
+    thisHome.navLinks = document.querySelectorAll(select.nav.links);
   }
 
   initWidgets(){
     const thisHome = this;
 
     thisHome.carouselWidget = new Carousel(thisHome.dom.carouselWrapper);
+  }
+
+  initActions(){
+    const thisHome = this;
+
+    const links = thisHome.element.querySelectorAll('a');
+
+    for(let link of links){
+      link.addEventListener('click', function(){
+        const linkId = link.getAttribute('href').replace('#', '');
+        thisHome.activatePage(linkId);
+        window.location.hash = `#/${linkId}`;
+      });
+    }
+  }
+
+  activatePage(pageId){
+    const thisHome = this;
+    for(let page of thisHome.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for(let link of thisHome.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == `#${pageId}`);
+    }
   }
 }
 
